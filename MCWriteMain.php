5e5ecba0292b5eadd2c61c379b010d5b86f1cf0d
DanielWilleke <drwilleke0624@yahoo.com>
2015-03-01 11:04:50 -0500

    Initial Commit of Skeleton Files
    Skeleton Files added to Repo

---
+++ MCWriteMain.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>MC Write</title>
+	<link rel="icon" type="image/png" href="http://pokeasaurus.com/favicon.png"/>
+	<link rel="stylesheet" href="dist/css/bootstrap.css"/>
+	<script>
+
+	</script>
+	<style type="text/css">
+
+	</style>
+</head>
+<body>
+
+<?php
+
+?>
+</body>
